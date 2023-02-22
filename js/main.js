c017ce016aa251b42277a41e24f35e204ebf71e9
AndreyMalaz <125899287+AndreyMalaz@users.noreply.github.com>
2023-02-22 19:40:58 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 
-//Burger Setup
+/Burger Setup
 
 const navBtn = document.querySelector('.nav__toggle');
 const menuIcon = document.querySelector('.menu-icon');
 const nav = document.querySelector('.nav');
 const body = document.body;
 
-navBtn.onclick = function(){
-	nav.classList.toggle('nav-mobile');
-    menuIcon.classList.toggle('menu-icon-active');
-	body.classList.toggle('lock');
 
+if (navBtn) {
+	menuIcon.addEventListener('click', e => {
+		nav.classList.toggle('nav-mobile')
+		menuIcon.classList.toggle('menu-icon-active')
+		body.classList.toggle('lock')
+	})
+	nav.querySelectorAll('.nav-link').forEach(link => {
+		link.addEventListener('click', () => {
+			nav.classList.remove('nav-mobile')
+			menuIcon.classList.remove('menu-icon-active')
+			body.classList.remove('lock')
+		})
+	})
 }
+
 
 //Setting up the animation
 
@@ -19,3 +29,4 @@ AOS.init({
 	//disable: 'phone'
 	//once: true
 });
+
